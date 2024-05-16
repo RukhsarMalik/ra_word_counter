b523dafc2439d86fd37ac190c9f7ca2a4a2126ff
RukhsarMalik <103738665+RukhsarMalik@users.noreply.github.com>
2024-05-16 18:50:58 +0500

    my first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt({
+    name: "sentences",
+    type: "input",
+    message: "Enter Your Sentence to count the words: "
+});
+const words = answer.sentences.trim().split(" ");
+console.log(words);
+console.log(`Your words length is ${words.length}`);
---
+++ main.ts
@@ -0,0 +1,14 @@
+import inquirer from "inquirer"
+const answer : 
+{ sentences : string} = await inquirer.prompt(
+    {
+        name : "sentences",
+        type: "input",
+        message : "Enter Your Sentence to count the words: "
+        
+
+    }
+);
+const words = answer.sentences.trim().split(" ");
+ console.log(words);
+ console.log(`Your words length is ${words.length}`)
